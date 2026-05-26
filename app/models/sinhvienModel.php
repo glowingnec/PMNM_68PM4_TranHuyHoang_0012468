0f807a2808f7a39d47e9d6dc471790462df506ac
Nectar <nectar.296@gmail.com>
2026-05-26 20:55:44 +0700

    feat<model>: add sinhvienModel::getAll()

---
+++ app/models/sinhvienModel.php
@@ -0,0 +1,26 @@
+<?php
+require_once '../app/core/DB.php';
+class sinhvienModel {
+    private $conn;
+
+    public function __construct() {
+        $db = new db();
+        $this->conn = $db->connect();
+    }
+    public function getAllSinhVien() {
+        $sql_query = "SELECT * FROM tbl_sinhvien";
+        $stmt = $this->conn->prepare($sql_query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function getSinhVienById($id) {
+        $sql_query = "SELECT * FROM tbl_sinhvien WHERE id = :id";
+        $stmt = $this->conn->prepare($sql_query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+}
+?>
+
+    
